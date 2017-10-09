61a09258b40114d558f7ae5d9a4edaf8a7e4fffa
James Hogan <jhogan@jhogan.net>
2017-10-08 17:07:37 -0700

    Initial release

---
+++ README.md
@@ -0,0 +1,29 @@
+Print the [CoinMarketCap](http://www.coinmarketcap.com) [USD price history](https://coinmarketcap.com/currencies/bitcoin/historical-data/) for a particular cryptocurrency in CSV format.
+
+Among other things, this is useful for US tax reporting.  If you want to know the cost basis for a trade (or for coins acquired through mining), the IRS requires you to denominate that cost basis in USD.  In the case of token-for-token trades (e.g. purchasing ETH with BTC), that requires you know the USD:BTC exchange rate at the time of the trade.
+
+Surprisingly, as of October 2017, it's not easy to get this data in a machine-readable format anywhere online.
+
+Rather than getting the exchange rate at the exact moment of your trade, which is generally not feasible, the IRS standard (at least for similar situations w/stock) is to use the average of a stock's high and low price for the day. CoinMarketCap doesn't provide this figure, but this tool calculates this number and includes it in the output.
+
+**Installation:**
+
+This script requires Python 2 to be available at /usr/bin/python.  This is the case by default with macOS.
+
+**Usage:**
+ 
+```
+./coinmarketcap-usd-history.py <currency> <start_year> <end_year>
+```
+
+Where:
+
+* `<currency>` is the (case-insensitive) name of the currency / token as displayed on CoinMarketCap, with dashes in place of spaces
+* `<start_year>` is the beginning of the range to fetch data for
+* `<end_year>` is the end of the range to fetch data for
+
+You can, of course, write results to a file with output redirection:
+
+```
+./coinmarketcap-usd-history.py <currency> <start_year> <end_year> > <output_filename>
+```
---
+++ coinmarketcap-usd-history.py
@@ -0,0 +1,123 @@
+#!/usr/bin/python
+
+"""
+CoinMarketCap USD Price History
+
+  Print the CoinMarketCap USD price history for a particular cryptocurrency in CSV format.
+"""
+
+
+import sys
+import re
+import urllib
+
+
+def parse_options():
+  """
+  Extract parameters from command line.
+  """
+
+  args = sys.argv[1:]
+
+  invalid_args = len(args) != 3
+
+  if len(args) >= 1: currency = args[0].lower()
+  if len(args) >= 2: start_year = args[1]
+  if len(args) >= 3: end_year = args[2]
+
+  # CoinMarketCap's price data (at least for Bitcoin, presuambly for all others) only goes back to 2013
+  invalid_args = invalid_args or int(start_year) < 2013
+  invalid_args = invalid_args or int(end_year) < 2013
+  invalid_args = invalid_args or int(end_year) < int(start_year)
+
+  if invalid_args:
+    print('Usage: ' + __file__ + ' <currency> <start_year> <end_year>');
+    sys.exit(1)
+
+  return currency, start_year, end_year
+
+
+def download_data(currency, start_year, end_year):
+  """
+  Download HTML price history for the specified cryptocurrency and time range from CoinMarketCap.
+  """
+
+  start_date = start_year + '0101'
+  end_date = end_year + '1231'
+  url = 'https://coinmarketcap.com/currencies/' + currency + '/historical-data/' + '?start=' + start_date + '&end=' + end_date
+
+  try:
+    page = urllib.urlopen(url)
+    if page.getcode() != 200:
+      raise Exception('Failed to load page') 
+    html = page.read()
+    page.close()
+
+  except:
+    print('Error fetching price data from ' + url)
+    print('Did you use a valid CoinMarketCap currency?\nIt should be entered exactly as displayed on CoinMarketCap.com (case-insensitive), with dashes in place of spaces.')
+    sys.exit(1)
+
+  return html
+
+
+def extract_data(html):
+  """
+  Extract the price history from the HTML.
+
+  The CoinMarketCap historical data page has just one HTML table.  This table contains the data we want.
+  It's got one header row with the column names.
+
+  We need to derive the "average" price for the provided data.
+  """
+
+  head = re.search(r'<thead>(.*)</thead>', html, re.DOTALL).group(1)
+  header = re.findall(r'<th .*>([\w ]+)</th>', head)
+  header.append('Average (High + Low / 2)')
+
+  body = re.search(r'<tbody>(.*)</tbody>', html, re.DOTALL).group(1)
+  raw_rows = re.findall(r'<tr[^>]*>\s*<td[^>]*>([^<]+)</td>\s*<td[^>]*>([^<]+)</td>\s*<td[^>]*>([^<]+)</td>\s*<td[^>]*>([^<]+)</td>\s*<td[^>]*>([^<]+)</td>\s*<td[^>]*>([^<]+)</td>\s*<td[^>]*>([^<]+)</td>\s*</tr>', body)
+
+  # strip commas
+  rows = []
+  for row in raw_rows:
+    row = [ field.translate(None, ',') for field in row ]
+    rows.append(row)
+
+  # calculate averages
+  def append_average(row):
+    high = float(row[header.index('High')])
+    low = float(row[header.index('Low')])
+    average = (high + low) / 2
+    row.append( '{:.2f}'.format(average) )
+    return row
+  rows = [ append_average(row) for row in rows ]
+
+  return header, rows
+
+
+def render_csv_data(header, rows):
+  """
+  Render the data in CSV format.
+  """
+  print(','.join(header))
+
+  for row in rows:
+    print(','.join(row))
+  
+  return
+
+
+def main():
+
+  currency, start_year, end_year = parse_options()
+  
+  html = download_data(currency, start_year, end_year)
+  header, rows = extract_data(html)
+  render_csv_data(header, rows)
+  
+  return
+
+
+if __name__ == '__main__':
+  main()
