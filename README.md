b00cf3a458082736d2a80d83b0ff040cdca47a91
Nazariy <nazariyv@users.noreply.github.com>
2017-10-18 06:20:20 +0100

    Returns a DataFrame if you call it from another python module (#4)
    * 1.Global Parser Instantiation (for the purposes of calling parser.arg_parse() from different python modules; and thus enabling us to return the dataframe); 2.Validation changed, since it is redundant with the introduction of the argparser. 3. Added functionality of returning a dataframe from another python module through the following: import coinmarketcap_usd_history; df = main(['bitcoin','2017','2017','--toDf']);.
    
    * Update README.md
    
    * Update README.md
    
    * Cosmetic changes
    
    * Returns a nicely processed dataFrame, everything is float except for the Date, which is converted into the datetime object
    
    * 1. dos2unix converted to unix line endings; 2. import pandas only when required.
    
    * Executable permissions for coinmarketcap_usd_history.py
    
    * Update README.md
    
    * 1.Custom start and end date. 2.Regex string validation. 3.Datetime validation.
    
    * Instead of printing the error message for the invalid format of the start and end dates, raise the Value Error and terminate the script
    
    * Update README.md
    
    * Update README.md

--- README.md
+++ README.md
@@ -14,21 +14,24 @@ Rather than getting the exchange rate at the exact moment of your trade, which i
 This script requires Python 2 to be available at /usr/bin/python.  This is the case by default with macOS.
 
 **Usage:**
- 
-```
-./coinmarketcap-usd-history.py <currency> <start_year> <end_year>
-```
 
+Just run in the terminal:
+```./coinmarketcap_usd_history.py <currency> <start_date> <end_date>```
+   
 **Where:**
 
 * `<currency>` is the (case-insensitive) name of the currency / token as displayed on CoinMarketCap, with dashes in place of spaces
-* `<start_year>` is the beginning of the range to fetch data for
-* `<end_year>` is the end of the range to fetch data for
+* `<start_date>` is the beginning of the range to fetch data for. For example, 2017-10-01 (for 2017 October 10th)
+* `<end_year>` is the end of the range to fetch data for. You may use the date in the future here to obtain the latest data. Format is the same as in the start date.
 
-The above information can also be found by running: `python coinmarketcap-usd-history.py -h` in your terminal.
+The above information can also be found by running: `python coinmarketcap_usd_history.py -h` in your terminal.
 
-You can, of course, write results to a file with output redirection:
+You can, of course, write the results to a file with the output redirection:
 
 ```
-./coinmarketcap-usd-history.py <currency> <start_year> <end_year> > <output_filename>
+./coinmarketcap_usd_history.py <currency> <start_date> <end_date> > <output_filename>
 ```
+**Using in another python module:**
+
+You can also use the module in other python modules to get back a pandas dataframe with the history of the crypto coin. You may need to add the path to `coinmarketcap_usd_history.py` in your `sys.path` through a command like the following: `sys.path.append(<path_to_coinmarketcap_usd_history.py_parent_folder>)` (alternatively, just move the `coinmarketcap_usd_history.py` file to the directory of your module that is to use `coinmarketcap_usd_history.py`). Also, import the module: `import coinmarketcap_usd_history`.Then perform this to obtain the dataframe: `df = coinmarketcap_usd_history.main(['bitcoin','2017','2017','--toDf'])`. Note that if you just wish to see the output of the call in another python module, simply omit the `'--toDf'` part, which acts as a boolean flag (i.e. if it is present, then it gets converted to `True`, otherwise `False`).
+  
---
+++ coinmarketcap_usd_history.py
@@ -0,0 +1,173 @@
+#!/usr/bin/python
+
+"""
+CoinMarketCap USD Price History
+
+  Print the CoinMarketCap USD price history for a particular cryptocurrency in CSV format.
+"""
+
+import sys
+import re
+import urllib2
+import argparse
+import datetime
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("currency",    help="This is the name of the crypto, as is shown on coinmarketcap. For BTC, "
+                                        "for example, type: bitcoin.", type=str)
+parser.add_argument("start_date",  help="Start date from which you wish to retrieve the historical data. For example, "
+                                        "'2017-10-01'.", type=str)
+parser.add_argument("end_date",    help="End date for the historical data retrieval. If you wish to retrieve all the "
+                                        "data then you can give a date in the future. Same format as in start_date "
+                                        "'yyyy-mm-dd'.", type=str)
+parser.add_argument("--dataframe", help="If present, returns a pandas DataFrame.",action='store_true')
+
+def parse_options(args):
+  """
+  Extract parameters from command line.
+  """
+
+  currency   = args.currency.lower()
+  start_date = args.start_date
+  end_date   = args.end_date
+
+  start_date_split = start_date.split('-')
+  end_date_split   = end_date.split('-')
+
+  start_year = int(start_date_split[0])
+  end_year   = int(end_date_split[0])
+
+  # String validation
+  pattern    = re.compile('[2][0][1][0-9]-[0-1][0-9]-[0-3][0-9]')
+  if not re.match(pattern, start_date):
+    raise ValueError('Invalid format for the start_date: ' + start_date + ". Should be of the form: yyyy-mm-dd.")
+  if not re.match(pattern, end_date):
+    raise ValueError('Invalid format for the end_date: '   + end_date   + ". Should be of the form: yyyy-mm-dd.")
+  # Datetime validation for the correctness of the date. Will throw a ValueError if not valid
+  datetime.datetime(start_year,int(start_date_split[1]),int(start_date_split[2]))
+  datetime.datetime(end_year,  int(end_date_split[1]),  int(end_date_split[2]))
+
+  # CoinMarketCap's price data (at least for Bitcoin, presuambly for all others) only goes back to 2013
+  invalid_args =                 start_year < 2013
+  invalid_args = invalid_args or end_year   < 2013
+  invalid_args = invalid_args or end_year   < start_year
+
+  if invalid_args:
+    print('Usage: ' + __file__ + ' <currency> <start_date> <end_date> --dataframe')
+    sys.exit(1)
+
+  start_date = start_date_split[0]+ start_date_split[1] + start_date_split[2]
+  end_date   = end_date_split[0]  + end_date_split[1]   + end_date_split[2]
+
+  return currency, start_date, end_date
+
+
+def download_data(currency, start_date, end_date):
+  """
+  Download HTML price history for the specified cryptocurrency and time range from CoinMarketCap.
+  """
+
+  url = 'https://coinmarketcap.com/currencies/' + currency + '/historical-data/' + '?start=' \
+                                                + start_date + '&end=' + end_date
+
+  try:
+    page = urllib2.urlopen(url,timeout=10)
+    if page.getcode() != 200:
+      raise Exception('Failed to load page') 
+    html = page.read()
+    page.close()
+
+  except Exception as e:
+    print('Error fetching price data from ' + url)
+    print('Did you use a valid CoinMarketCap currency?\nIt should be entered exactly as displayed on CoinMarketCap.com (case-insensitive), with dashes in place of spaces.')
+    
+    if hasattr(e, 'message'):
+      print("Error message: " + e.message)
+    else:
+      print(e)
+      sys.exit(1)
+
+  return html
+
+
+def extract_data(html):
+  """
+  Extract the price history from the HTML.
+
+  The CoinMarketCap historical data page has just one HTML table.  This table contains the data we want.
+  It's got one header row with the column names.
+
+  We need to derive the "average" price for the provided data.
+  """
+
+  head = re.search(r'<thead>(.*)</thead>', html, re.DOTALL).group(1)
+  header = re.findall(r'<th .*>([\w ]+)</th>', head)
+  header.append('Average (High + Low / 2)')
+
+  body = re.search(r'<tbody>(.*)</tbody>', html, re.DOTALL).group(1)
+  raw_rows = re.findall(r'<tr[^>]*>' + r'\s*<td[^>]*>([^<]+)</td>'*7 + r'\s*</tr>', body)
+
+  # strip commas
+  rows = []
+  for row in raw_rows:
+    row = [ field.translate(None, ',') for field in row ]
+    rows.append(row)
+
+  # calculate averages
+  def append_average(row):
+    high = float(row[header.index('High')])
+    low = float(row[header.index('Low')])
+    average = (high + low) / 2
+    row.append( '{:.2f}'.format(average) )
+    return row
+  rows = [ append_average(row) for row in rows ]
+
+  return header, rows
+
+
+def render_csv_data(header, rows):
+  """
+  Render the data in CSV format.
+  """
+  print(','.join(header))
+
+  for row in rows:
+    print(','.join(row))
+
+# --------------------------------------------- Util Methods -----------------------------------------------------------
+
+def processDataFrame(df):
+  import pandas as pd
+  assert isinstance(df, pd.DataFrame), "df is not a pandas DataFrame."
+
+  cols = list(df.columns.values)
+  cols.remove('Date')
+  df.loc[:,'Date'] = pd.to_datetime(df.Date)
+  for col in cols: df.loc[:,col] = df[col].apply(lambda x: float(x))
+  return df.sort_values(by='Date').reset_index(drop=True)
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+def main(args=None):
+  # assert that args is a list
+  if(args is not None):
+    args = parser.parse_args(args)
+  else:
+    args = parser.parse_args()
+  
+  currency, start_date, end_date = parse_options(args)
+  
+  html = download_data(currency, start_date, end_date)
+  
+  header, rows = extract_data(html) 
+  
+  if(args.dataframe):
+    import pandas as pd
+    return processDataFrame(pd.DataFrame(data=rows,columns=header))
+  else:  
+    render_csv_data(header, rows)
+
+
+if __name__ == '__main__':
+  df = main()
