ce18397aa996fb555b93a3a3032bbfcbb43a52de
Thomas <mc_teo@live.ie>
2019-05-12 12:53:53 +0100

    Improved cross-compatibility of installed shell script.
    Using 'scripts' in the setup.py is not cross-compatible (esp. for
    Windows). Using 'entry_points' allows setuptools to automatically
    generate a wrapper shell script or .exe depending on the platform.

--- setup.py
+++ setup.py
@@ -13,7 +13,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Alescontrela/coinmarketcap-history",
     packages=setuptools.find_packages(),
-    scripts=['bin/coinmarketcap'],
+    entry_points={
+        "console_scripts": [
+            "coinmarketcap=cmc.coinmarketcap:main",
+        ],
+    },
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
